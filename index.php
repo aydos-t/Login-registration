3fc5671756f2a14099ab0c15d7f261d040de9471
Айдос Тажиниязов <a.tazhiniazov@gmail.com>
2023-09-05 17:37:49 +0500

    add navbar

--- index.php
+++ index.php
@@ -1,17 +1,27 @@
-<?php 
-// TODO: includes components
+<?php
+require_once 'components/navbar.php';
 ?>
 
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+    <link rel="stylesheet" href="assets/css/bootstrap5.min.css">
+    <link rel="stylesheet" href="assets/css/custom.css">
+</head>
+<body>
 <div class="py-5">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h3>Hello</h3>
+                <h3>Home Page</h3>
             </div>
         </div>
     </div>
 </div>
-
-<?php
-// TODO: includes components
-?>
+</body>
+</html>
