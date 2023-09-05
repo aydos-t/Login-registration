7b556227cc8383dabb4710313bc6cec9b86e6359
Айдос Тажиниязов <a.tazhiniazov@gmail.com>
2023-09-05 15:03:04 +0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+// TODO: includes components
+?>
+
+<div class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h3>Hello</h3>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+// TODO: includes components
+?>
