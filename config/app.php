4110a29bde1f7f534945d56c463bf72da19890fb
Айдос Тажиниязов <a.tazhiniazov@gmail.com>
2023-09-07 20:56:17 +0500

    add register.php file

--- config/app.php
+++ config/app.php
@@ -1,10 +1,23 @@
 <?php
 
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASSWORD','');
-define('DB_DATABASE','login-register-php-oop');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'login-register-php-oop');
 
-include_once ('DatabaseConnection.php');
+define('SITE_URL', 'http://login-register-php-oop/');
+
+include_once('DatabaseConnection.php');
 $db = new DatabaseConnection;
+
+function base_url($slug)
+{
+    echo SITE_URL . $slug;
+}
+
+function validateInput($dbcon,$input)
+{
+    return mysqli_real_escape_string($dbcon,$input);
+}
+
 ?>
---
+++ register.php
@@ -0,0 +1,59 @@
+<?php
+require_once 'components/navbar.php';
+require_once 'auth/Authentication.php';
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register</title>
+    <link rel="stylesheet" href="assets/css/bootstrap5.min.css">
+    <link rel="stylesheet" href="assets/css/custom.css">
+</head>
+<body>
+<div class="py-5">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-5">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Register</h4>
+                    </div>
+                    <div class="card-body">
+                        <form action="" method="POST">
+                            <div class="form-group mb-3">
+                                <label>Name</label>
+                                <input type="text" name="name" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                                <label>SurName</label>
+                                <input type="text" name="surName" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                                <label>Email</label>
+                                <input type="email" name="email" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                                <label>Password</label>
+                                <input type="password" name="password" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                                <label>Confirm Password</label>
+                                <input type="password" name="confirm_password" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                                <button type="submit" name="register_btn" class="btn btn-primary">Register</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
