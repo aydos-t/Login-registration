8a153df74b76c8ad39802a54c76d62d4886d70df
Айдос Тажиниязов <a.tazhiniazov@gmail.com>
2023-09-11 10:11:33 +0500

    modified links

--- components/navbar.php
+++ components/navbar.php
@@ -1,7 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container">
-<!--        <a class="navbar-brand" href="--><?//= base_url('index.php') ?><!--">Home</a>-->
-        <a class="navbar-brand" href="../index.php">Home</a>
+        <a class="navbar-brand" href="<?= base_url('index.php') ?>">Home</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -9,12 +8,10 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="<?= base_url('login.php') ?>">Login</a>
-<!--                    <a class="nav-link" href="../login.php">Login</a>-->
+                    <a class="nav-link" href="<?= base_url('../login.php') ?>">Login</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="<?= base_url('../register.php') ?>">Register</a>
-<!--                    <a class="nav-link" href="../register.php">Register</a>-->
                 </li>
             </ul>
         </div>
