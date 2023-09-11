ce499b73dfe5e88270c4c0fe7c45b230aaccc1da
Айдос Тажиниязов <a.tazhiniazov@gmail.com>
2023-09-11 17:29:00 +0500

    add user profile

---
+++ my-profile.php
@@ -0,0 +1,26 @@
+<?php
+require_once "controllers/AuthenticationController.php";
+require_once 'components/navbar.php';
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+    <link rel="stylesheet" href="assets/css/bootstrap5.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
+            crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="py-5">
+    <div class="container">
+        <h3>My Profile</h3>
+    </div>
+</div>
+</body>
+</html>
